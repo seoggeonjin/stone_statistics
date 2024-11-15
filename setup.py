36069c18fac56b147a785645168cb485023923bf
BuildTools <unconfigured@null.spigotmc.org>
2024-11-15 16:01:06 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stones_statistics",  # Replace with your own username
-    version="0.1.0",
+    version="0.1.1",
     author="Stone",
     author_email="seoggeonjin@gmail.com",
     install_requires=[
